411ef8b44e5e0fc4d5f9a8ecb21225b4ba22224a
awhooshingwind <awhooshingwind@gmail.com>
2024-09-04 15:41:10 -0700

    comment cleanup

--- trigger_seq.py
+++ trigger_seq.py
@@ -2,14 +2,18 @@ import pypylon.pylon as py
 import pypylon.genicam as geni
 import matplotlib.pyplot as plt
 import numpy as np
-import cv2
+# import cv2
 import os
 
 import time
 import datetime
 import queue
 
-# Define class for storing 3 images and doing img calc
+""" Define class for storing/displaying 3 triggered images, 
+using image sequence to calculate corrected image,
+then saving the resulting image as .txt file for analysis
+Requires pypylon
+"""
 class TriggeredSequence:
     def __init__(self, save_path='Basler', autosave=False):
         self.images = {}
@@ -19,6 +23,7 @@ class TriggeredSequence:
         self.autosave = autosave
         self.sequence_count = 1
 
+
     def add_image(self, image):
         self.images[self.image_types[self.image_count]] = image
         self.image_count += 1
@@ -26,12 +31,14 @@ class TriggeredSequence:
             self.sequence_complete()
             self.image_count = 0
     
+
     def sequence_complete(self):
         self.calc_result()
-        if not self.autosave:
+        if self.autosave:
             self.save_result()
         self.display_images()
         self.sequence_count += 1
+
 
     def calc_result(self):
         shallow = self.images['shallow']
@@ -46,15 +53,11 @@ class TriggeredSequence:
              (shallow <= dark) | ((light - dark) < (shallow - dark)), 1,
              np.round(1000.0 * np.log((light - dark) / (shallow - dark))).astype(np.uint16)
         )
-        
         # handle NaN values for testing
         result = np.nan_to_num(result, nan=0, posinf=0, neginf=0).astype(np.uint16)
-        
-        if self.autosave:
-            self.save_result(result)
-        
         self.images['calc'] = result
-    
+
+
     def save_result(self):
         data = self.images['calc']
         # Make dir if necessary
@@ -62,10 +65,7 @@ class TriggeredSequence:
             os.makedirs(self.save_path)
         ## Edit filename convention here ## 
         timestamp = datetime.datetime.now().strftime('%m%d')
-        # as is - overwrites existing images each session for testing
         base_name = f"{self.save_path}/b{timestamp}{self.sequence_count}"
-        # save as .tif
-        # cv2.imwrite(base_name+'.tif', data)
         # save as .txt (in specified format)
         h,w = data.shape
         header = (f"resx 1 widthx {w} centerx {w//2} "
@@ -93,14 +93,12 @@ class TriggeredSequence:
         for i, (k, v) in enumerate(self.images.items()):
             plt.figure(figsize=(10,5))
             plt.imshow(v, cmap='viridis')
-            # print(v.shape)
             plt.clim(vmin=np.min(v), vmax=np.max(v))
             plt.title(k)
             plt.axis('off')
             plt.colorbar()
         plt.pause(0.001) # plt.pause so non-blocking
         plt.draw()
-        # plt.show()
 
 
 # open the camera
@@ -112,20 +110,18 @@ cam.Open()
 cam.UserSetSelector.Value = "Default"
 cam.UserSetLoad.Execute()
 
-# Hardware Trigger Config
-# setup the io section
-# cam.LineSelector.Value = "Line1"
-# cam.LineMode.Value = "Input"
-
 # # setup the trigger / acquisition controls
-
 cam.TriggerSelector.Value = "FrameStart"
-# cam.TriggerSource.Value = "Line1"
 cam.TriggerMode.Value = "On"
-# print(cam.TriggerActivation.Value)
 
 # Software trigger config (for testing)
 cam.TriggerSource.Value = "Software"
+
+# Hardware Trigger Config:
+# cam.TriggerSource.Value = "Line1"
+# cam.LineSelector.Value = "Line1"
+# cam.LineMode.Value = "Input"
+# print(cam.TriggerActivation.Value)
 
 # definition of event handler class 
 class TriggeredImage(py.ImageEventHandler):
@@ -149,24 +145,23 @@ cam.RegisterImageEventHandler(image_handler,
                               py.RegistrationMode_Append, 
                               py.Cleanup_None)
 
+# Helper function for software triggering on user input
 def getkey():
     return input("Enter \"t\" to trigger the camera or \"e\" to exit and press enter? (t/e) ")
 
 try:
-    # start grabbing with background loop (hardware trigger)
-    cam.StartGrabbingMax(100, py.GrabStrategy_LatestImages, py.GrabLoop_ProvidedByInstantCamera)
-    # Start the grabbing using the grab loop thread, by setting the grabLoopType parameter
-    # to GrabLoop_ProvidedByInstantCamera. The grab results are delivered to the image event handlers.
-    # The GrabStrategy_OneByOne default grab strategy is used.
-    # cam.StartGrabbing(py.GrabStrategy_OneByOne, py.GrabLoop_ProvidedByInstantCamera)
+    # start grabbing with background loop 
+    # cam.StartGrabbingMax(100, py.GrabStrategy_LatestImages, py.GrabLoop_ProvidedByInstantCamera) # testing software trigger
+    cam.StartGrabbing(py.GrabStrategy_OneByOne, py.GrabLoop_ProvidedByInstantCamera) # hardware trigger
 
     while True:
-    # wait ... or do something relevant
-        time.sleep(0.05)
+        time.sleep(0.1)
         # Process Images
         if not image_queue.empty():
             img = image_queue.get()
             trigger_sequence.add_image(img)
+
+        # For software triggering
         key = getkey()
         # print(key)
         if key.lower() == 't':
